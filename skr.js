b865aaefd937aea8b334e30283cd7d831765edfd
TaniaMed <ikrendelek@mail.ru>
2017-04-07 21:54:20 +0300

    Update skr.js

--- skr.js
+++ skr.js
@@ -3,22 +3,21 @@ let step = 1;
 setInterval(() => {
     switch(step) {
         case 1: 
-            img.src = "img/airmax.png";
+            img.src = "https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-jump.png";
             break;
         case 2: 
-            img.src = "img/airmax-jump.png";
+            img.src = "https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-on-foot.png";
             break;
         case 3: 
-            img.src = "img/airmax-on-foot.png";
+            img.src = "https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-playground.png";
             break;
         case 4: 
-            img.src = "img/airmax-playground.png";
+            img.src = "https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-top-view.png";
             break;
         case 5: 
-            img.src = "img/airmax-top-view.png";
+            img.src = "https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax.png";
             break;
-    }
-        
+    }        
         step += 1;
         step %= 6;
-      }, 5000);
+}, 5000);
